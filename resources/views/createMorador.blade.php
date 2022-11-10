4e62c03e1448640a90cd41c0db83d44eba2297e2
Joao Victor <jvribascoelho@gmail.com>
2022-11-10 00:10:03 -0300

    edicao createMorador

--- resources/views/createMorador.blade.php
+++ resources/views/createMorador.blade.php
@@ -1,12 +1,9 @@
 @extends('templates.template')
 
 @section('content')
-    
-<h1 class = "text-center">Cadastrar Morador</h1>
-
 
 @if( Request::is('*/edit'))
-<h1 class = "text-center">Editar Apartamento</h1>
+<h1 class = "text-center">Editar Morador</h1>
 
 <form action = "{{ route('update_morador', $morador->id) }}" method = "post">
     @csrf 
@@ -36,7 +33,8 @@
     <button type="submit" class="btn btn-dark">Salvar</button>
     
     @else
-    
+    <h1 class = "text-center">Cadastrar Morador</h1>
+
     <form action = "{{ route('salvar_morador') }}" method = "post">
     @csrf 
     <div class="form-group">
